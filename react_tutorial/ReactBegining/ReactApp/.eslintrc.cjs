ea94aff80d0dabb7ce7980baf7701327c6763a01
leven.dmitry <leven.dmitry@gmail.com>
2023-09-08 13:35:14 +0300

    initial commit

---
+++ react_tutorial/ReactBegining/ReactApp/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'eslint-config-prettier',
+  ],
+  settings: {
+    react: {
+      // Tells eslint-plugin-react to automatically detect the version of React to use.
+      version: 'detect',
+    },
+    // Tells eslint how to resolve imports
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'semi': [1, 'always'],
+  },
+}
---
+++ react_tutorial/ReactBegining/ReactApp/src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div>Hello World{console.log('11111')}</div>;
+}
+
+export default App;
